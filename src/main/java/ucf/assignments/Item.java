cb208b5fdc010043038b43c77fbcbce8411d1be5
xera51 <xera51@protonmail.com>
2021-07-07 15:55:15 -0400

    Updated Item

--- src/main/java/ucf/assignments/Item.java
+++ src/main/java/ucf/assignments/Item.java
@@ -11,25 +11,47 @@ import java.time.LocalDate;
 
 public class Item {
 
-    private StringProperty description = new SimpleStringProperty("");
-    private ObjectProperty<LocalDate> dueDate = new SimpleObjectProperty<>(LocalDate.now());
-    private BooleanProperty complete = new SimpleBooleanProperty(false);
+    private final StringProperty description = new SimpleStringProperty("");
+    private final ObjectProperty<LocalDate> dueDate = new SimpleObjectProperty<>(LocalDate.now());
+    private final BooleanProperty complete = new SimpleBooleanProperty(false);
 
     public Item() {}
+
     public Item(String description, LocalDate dueDate) {
         this.setDesc(description);
         this.setDueDate(dueDate);
     }
 
-    public String getDesc() { return this.description.get(); }
-    public LocalDate getDueDate() { return this.dueDate.get(); }
-    public boolean isComplete() { return this.complete.get(); }
+    public Item(String description, LocalDate dueDate, boolean complete) {
+        this.setDesc(description);
+        this.setDueDate(dueDate);
+        this.setComplete(complete);
+    }
 
-    public void setDesc(String description) { this.description.set(description); }
-    public void setDueDate(LocalDate dueDate) { this.dueDate.set(dueDate); }
-    public void setComplete(boolean complete) { this.complete.set(complete); }
+    public final String getDesc() { return this.description.get(); }
+    public final LocalDate getDueDate() { return this.dueDate.get(); }
+    public final boolean isComplete() { return this.complete.get(); }
+
+    public final void setDesc(String description) { this.description.set(description); }
+    public final void setDueDate(LocalDate dueDate) { this.dueDate.set(dueDate); }
+    public final void setComplete(boolean complete) { this.complete.set(complete); }
 
     public StringProperty descriptionProperty() { return this.description; }
     public ObjectProperty<LocalDate> dueDateProperty() { return this.dueDate; }
     public BooleanProperty completeProperty() { return this.complete; }
+
+    @Override
+    public String toString() {
+        return String.format("Due: %s%nDescription: %s%nComplete: %b%n",
+                this.getDueDate(), this.getDesc(), this.isComplete());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) return true;
+        if(!(obj instanceof Item i)) return false;
+        return this.getDesc().equals(i.getDesc())
+                && this.getDueDate().equals(i.getDueDate())
+                && (this.isComplete() == i.isComplete());
+    }
 }
